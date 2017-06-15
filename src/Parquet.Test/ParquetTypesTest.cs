eb1c886f67f10c3c18e914d1e568e81cc7ca2d92
Richard Conway <richard@elastacloud.com>
2017-06-15 07:16:47 +0100

    added new unit test for variable length byte array

--- src/Parquet.Test/ParquetTypesTest.cs
+++ src/Parquet.Test/ParquetTypesTest.cs
@@ -16,7 +16,7 @@ namespace Parquet.Test
           byte[] parquetBlock = utils.IntToLittleEndian(999);
 
           var encoding = new ParquetEncoding();
-          var output = encoding.ReadPlain<int>(parquetBlock, ParquetTypes.Type.Int32, 3);
+          var output = encoding.ReadPlain<int>(parquetBlock, Thrift.Type.INT32, 3);
 
           Assert.Equal(999, output);
        }
@@ -28,7 +28,7 @@ namespace Parquet.Test
           byte[] parquetBlock = utils.LongToLittleEndian(9999);
 
           var encoding = new ParquetEncoding();
-          var output = encoding.ReadPlain<long>(parquetBlock, ParquetTypes.Type.Int64, 4);
+          var output = encoding.ReadPlain<long>(parquetBlock, Thrift.Type.INT64, 4);
 
           Assert.Equal(9999, output);
        }
@@ -40,7 +40,7 @@ namespace Parquet.Test
           byte[] parquetBlock = utils.FloatToLittleEndian(9999.999f);
 
           var encoding = new ParquetEncoding();
-          var output = encoding.ReadPlain<float>(parquetBlock, ParquetTypes.Type.Float, 8);
+          var output = encoding.ReadPlain<float>(parquetBlock, Thrift.Type.FLOAT, 8);
 
           Assert.Equal(9999.999f, output);
        }
@@ -52,7 +52,7 @@ namespace Parquet.Test
           byte[] parquetBlock = utils.DoubleToLittleEndian(9999.9999999D);
 
           var encoding = new ParquetEncoding();
-          var output = encoding.ReadPlain<double>(parquetBlock, ParquetTypes.Type.Double, 12);
+          var output = encoding.ReadPlain<double>(parquetBlock, Thrift.Type.DOUBLE, 12);
 
           Assert.Equal(9999.9999999D, output);
        }
@@ -64,7 +64,7 @@ namespace Parquet.Test
           var bitArray = new BitArray(boolArray);
 
           var encoding = new ParquetEncoding();
-          var output = encoding.ReadPlain<bool[]>(new byte[] {bitArray.GetByte()}, ParquetTypes.Type.Boolean, 4);
+          var output = encoding.ReadPlain<bool[]>(new byte[] {bitArray.GetByte()}, Thrift.Type.BOOLEAN, 4);
 
           Assert.Equal(boolArray, output);
       }
@@ -75,11 +75,22 @@ namespace Parquet.Test
           byte[] parquetBlock = new byte[] { 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
          
           var encoding = new ParquetEncoding();
-          var output = encoding.ReadPlain<BigInteger>(parquetBlock, ParquetTypes.Type.Int96, 1);
+          var output = encoding.ReadPlain<BigInteger>(parquetBlock, Thrift.Type.INT96, 1);
 
           var bi = new BigInteger(65535D);
 
           Assert.Equal(bi, output);
+       }
+
+       [Fact]
+       public void TestByteArray()
+       {
+          byte[] parquetBlock = new byte[] { 1, 0, 0, 0, 67, 68, 69, 70 };
+
+          var encoding = new ParquetEncoding();
+          var output = encoding.ReadPlain<byte[]>(parquetBlock, Thrift.Type.BYTE_ARRAY, 1);
+
+          Assert.Equal(new byte[] { 67 }, output);
        }
 
       private string GetDataFilePath(string name)
