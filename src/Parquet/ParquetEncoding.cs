6bca3bca17cd25e0ba142df854816a67909bef78
Richard Conway <richard@elastacloud.com>
2017-06-17 13:19:42 +0100

    added date and timestamp types

--- src/Parquet/ParquetEncoding.cs
+++ src/Parquet/ParquetEncoding.cs
@@ -4,20 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using Parquet.Thrift;
 
 namespace Parquet
 {
     class ParquetEncoding
     {
-       public T ReadPlain<T>(byte[] parquetBlock, Thrift.Type type, int count, int width = 0)
+       public T ReadPlain<T>(byte[] parquetBlock, Thrift.Type type, Thrift.ConvertedType? convertedType = null, int count = 0, int width = 0)
        {
          // This is a poor implementation we have the width so we should walk the array - boxing/unboxing and casting ugly, slow and bloaty!!
           switch (type)
           {
              case Thrift.Type.INT32:
-                return (T) (object) BitConverter.ToInt32(parquetBlock, 0);
+                if (!convertedType.HasValue || convertedType.Value != ConvertedType.DATE)
+                   return (T) (object) BitConverter.ToInt32(parquetBlock, 0);
+                int epochInt = BitConverter.ToInt32(parquetBlock, 0);
+                return (T) (object) epochInt.FromUnixTime();
              case Thrift.Type.INT64:
-                return (T) (object) BitConverter.ToInt64(parquetBlock, 0);
+                if (!convertedType.HasValue || convertedType.Value != ConvertedType.TIMESTAMP_MILLIS)
+                   return (T) (object) BitConverter.ToInt64(parquetBlock, 0);
+                long epoch = BitConverter.ToInt64(parquetBlock, 0);
+                return (T) (object) epoch.FromUnixTime();
              case Thrift.Type.INT96:
                 return (T) (object) new BigInteger(parquetBlock);
              case Thrift.Type.FLOAT:
@@ -28,7 +35,8 @@ namespace Parquet
                 return (T) (object) new BitArray(parquetBlock).ConvertToBoolArray(count);
              case Thrift.Type.BYTE_ARRAY:
                 int elementCount = ParquetUtils.GetByteArrayLELength(parquetBlock.Take(4).ToArray());
-                return (T) (object) parquetBlock.Skip(4).Take(elementCount).ToArray(); 
+                return (T) (object) parquetBlock.Skip(4).Take(elementCount).ToArray();
+
              default:
                 return default(T);
           }
--- src/Parquet/ParquetUtils.cs
+++ src/Parquet/ParquetUtils.cs
@@ -33,7 +33,25 @@ namespace Parquet
           return output;
        }
 
-       
+       public static DateTime FromUnixTime(this int unixTime)
+       {
+          var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+          return epoch.AddSeconds(unixTime);
+       }
+
+       public static DateTime FromUnixTime(this long unixTime)
+       {
+          var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+          return epoch.AddSeconds(unixTime);
+       }
+
+      public static long ToUnixTime(this DateTime date)
+       {
+          var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+          return Convert.ToInt64((date - epoch).TotalSeconds);
+       }
+
+
    }
 
    public class NumericUtils
