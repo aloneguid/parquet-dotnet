ad97b2c3e61bd7e0619e9b5cce06076ba6cc2deb
Lucas Pickup <lupickup@microsoft.com>
2018-10-26 16:49:11 -0700

    Add support for reading ByteArray DataType. Add some tests for ByteArray columns.

--- src/Parquet/Data/Concrete/ByteArrayDataTypeHandler.cs
+++ src/Parquet/Data/Concrete/ByteArrayDataTypeHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -8,13 +9,62 @@ namespace Parquet.Data.Concrete
 {
    class ByteArrayDataTypeHandler : BasicDataTypeHandler<byte[]>
    {
+      private static readonly ArrayPool<byte> _bytePool = ArrayPool<byte>.Shared;
+      private static readonly ArrayPool<byte[]> _byteArrayPool = ArrayPool<byte[]>.Shared;
+
       public ByteArrayDataTypeHandler() : base(DataType.ByteArray, Thrift.Type.BYTE_ARRAY)
       {
       }
 
       public override Array GetArray(int minCount, bool rent, bool isNullable)
       {
-         throw new NotImplementedException();
+         if (rent)
+         {
+            return _byteArrayPool.Rent(minCount);
+         }
+
+         return new byte[minCount][];
+      }
+
+      public override bool IsMatch(Thrift.SchemaElement tse, ParquetOptions formatOptions)
+      {
+         return tse.__isset.type && tse.Type == Thrift.Type.BYTE_ARRAY
+                                 && !tse.__isset.converted_type;
+      }
+
+      public override int Read(BinaryReader reader, Thrift.SchemaElement tse, Array dest, int offset, ParquetOptions formatOptions)
+      {
+         int remLength = (int)(reader.BaseStream.Length - reader.BaseStream.Position);
+
+         if (remLength == 0)
+            return 0;
+
+         byte[][] tdest = (byte[][])dest;
+
+         //reading byte[] one by one is extremely slow, read all data
+
+         byte[] allBytes = _bytePool.Rent(remLength);
+         reader.BaseStream.Read(allBytes, 0, remLength);
+         int destIdx = offset;
+         try
+         {
+            Span<byte> span = allBytes.AsSpan(0, remLength);   //will be passed as input in future versions
+
+            int spanIdx = 0;
+
+            while (spanIdx < span.Length && destIdx < tdest.Length)
+            {
+               int length = span.Slice(spanIdx, 4).ReadInt32();
+               tdest[destIdx++] = span.Slice(spanIdx + 4, length).ToArray();
+               spanIdx = spanIdx + 4 + length;
+            }
+         }
+         finally
+         {
+            _bytePool.Return(allBytes);
+         }
+
+         return destIdx - offset;
       }
 
       public override Array MergeDictionary(Array dictionary, int[] indexes)
