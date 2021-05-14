c2583a2ce25b089d83bd350eba1fe778596b0048
Marat Faskhiev <marat.faskhiev@gmail.com>
2021-05-14 18:28:23 +0500

    Preserve ticks during the conversion from NanoTime to DateTimeOffset

---
+++ src/Parquet.Test/File/Values/Primitives/NanoTimeTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Parquet.File.Values.Primitives;
+using Xunit;
+
+namespace Parquet.Test.File.Values.Primitives
+{
+   public class NanoTimeTest
+   {
+      [Fact]
+      public void ConvertToDateTimeOffset_PreservesTicks()
+      {
+         var dto = new DateTimeOffset(2021, 05, 14, 17, 52, 31, TimeSpan.Zero);
+         dto = dto.Add(TimeSpan.FromTicks(1234567));
+         var nanoTime = new NanoTime(dto);
+         var convertedDto = (DateTimeOffset) nanoTime;
+
+         Assert.Equal(dto, convertedDto);
+      }
+   }
+}
