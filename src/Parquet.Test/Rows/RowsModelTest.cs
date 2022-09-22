b82136f6ee565afc8d89033a23556684c72fe914
IG <aloneguid@outlook.com>
2022-09-22 11:59:31 +0100

    bugger

--- src/Parquet.Test/Rows/RowsModelTest.cs
+++ src/Parquet.Test/Rows/RowsModelTest.cs
@@ -318,7 +318,7 @@ namespace Parquet.Test.Rows {
             t.Add(1, new[] { new Row(1, "Joe"), new Row(2, "Bloggs") });
             t.Add(2, new[] { new Row(3, "Star"), new Row(4, "Wars") });
 
-            Table t2 = await WriteReadAsync(t, true);
+            Table t2 = await WriteReadAsync(t);
 
             Assert.Equal(t.ToString(), t2.ToString(), ignoreLineEndingDifferences: true);
         }
