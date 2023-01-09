16f9285eeb98fb0e87711a4e7ec15374c50c43c0
IG <aloneguid@outlook.com>
2023-01-09 11:04:26 +0000

    attributes moved to Parquet namespace

--- src/Parquet/Serialization/SchemaReflector.cs
+++ src/Parquet/Serialization/SchemaReflector.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Parquet.Attributes;
 using Parquet.Data;
 using Parquet.Schema;
 
