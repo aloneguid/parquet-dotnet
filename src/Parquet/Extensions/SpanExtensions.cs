45359415b03524f13af9cee6341252c05817052b
IG <aloneguid@outlook.com>
2023-01-09 12:08:26 +0000

    DateTime and DateTimeOffset encoders

--- src/Parquet/Extensions/SpanExtensions.cs
+++ src/Parquet/Extensions/SpanExtensions.cs
@@ -64,5 +64,27 @@ namespace System {
                     max = i;
             }
         }
+
+        public static void MinMax(this ReadOnlySpan<DateTime> span, out DateTime min, out DateTime max) {
+            min = span.IsEmpty ? default(DateTime) : span[0];
+            max = min;
+            foreach(DateTime i in span) {
+                if(i < min)
+                    min = i;
+                if(i > max)
+                    max = i;
+            }
+        }
+
+        public static void MinMax(this ReadOnlySpan<DateTimeOffset> span, out DateTimeOffset min, out DateTimeOffset max) {
+            min = span.IsEmpty ? default(DateTimeOffset) : span[0];
+            max = min;
+            foreach(DateTimeOffset i in span) {
+                if(i < min)
+                    min = i;
+                if(i > max)
+                    max = i;
+            }
+        }
     }
 }
