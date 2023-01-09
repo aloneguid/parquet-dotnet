0817e394345fa5b2aa17527456c85b63590db992
IG <aloneguid@outlook.com>
2023-01-09 11:14:37 +0000

    compile errors

--- src/Parquet.Test/Serialisation/ParquetConvertTest.cs
+++ src/Parquet.Test/Serialisation/ParquetConvertTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Parquet.Attributes;
 using Parquet.Schema;
 using Parquet.Serialization;
 using Xunit;
