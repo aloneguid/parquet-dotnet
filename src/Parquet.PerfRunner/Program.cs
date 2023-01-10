c60a81dc05b883237e3171a53f0f5ac6e280b70b
IG <aloneguid@outlook.com>
2023-01-10 15:09:40 +0000

    testing and optimisations

--- src/Parquet.PerfRunner/Program.cs
+++ src/Parquet.PerfRunner/Program.cs
@@ -1,28 +1,6 @@
 ﻿// for performance tests only
 
-using Parquet;
-using Parquet.Data;
-using Parquet.Schema;
 using Parquet.PerfRunner.Benchmarks;
 
 //new VsParquetSharp().Main();
-await SimpleWrite();
-
-async Task SimpleWrite() {
-    
-    // allocate stream large enough to avoid re-allocations during performance test
-    const int l = 10000000;
-    var ms = new MemoryStream(l * sizeof(int) * 2);
-    var schema = new ParquetSchema(new DataField<int>("id"));
-    var rnd = new Random();
-    int[] ints = new int[l];
-    for(int i = 0; i < l; i++) {
-        ints[i] = rnd.Next();
-    }
-
-    using(ParquetWriter writer = await ParquetWriter.CreateAsync(schema, ms)) {
-        using(ParquetRowGroupWriter g = writer.CreateRowGroup()) {
-            await g.WriteColumnAsync(new DataColumn((DataField)schema[0], ints));
-        }
-    }
-}
+await new DataTypes().SimpleStringWriteRead();
