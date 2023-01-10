29dadc49e61f8b332806a720791cf17d0ab8017c
IG <aloneguid@outlook.com>
2023-01-10 10:28:23 +0000

    byte[] decoder

--- src/Parquet.PerfRunner/Program.cs
+++ src/Parquet.PerfRunner/Program.cs
@@ -3,19 +3,26 @@
 using Parquet;
 using Parquet.Data;
 using Parquet.Schema;
+using Parquet.PerfRunner.Benchmarks;
 
-// allocate stream large enough to avoid re-allocations during performance test
-const int l = 10000000;
-var ms = new MemoryStream(l * sizeof(int) * 2);
-var schema = new ParquetSchema(new DataField<int>("id"));
-var rnd = new Random();
-int[] ints = new int[l];
-for(int i = 0; i < l; i++) {
-    ints[i] = rnd.Next();
-}
+//new VsParquetSharp().Main();
+await SimpleWrite();
 
-using(ParquetWriter writer = await ParquetWriter.CreateAsync(schema, ms)) {
-    using(ParquetRowGroupWriter g = writer.CreateRowGroup()) {
-        await g.WriteColumnAsync(new DataColumn((DataField)schema[0], ints));
+async Task SimpleWrite() {
+    
+    // allocate stream large enough to avoid re-allocations during performance test
+    const int l = 10000000;
+    var ms = new MemoryStream(l * sizeof(int) * 2);
+    var schema = new ParquetSchema(new DataField<int>("id"));
+    var rnd = new Random();
+    int[] ints = new int[l];
+    for(int i = 0; i < l; i++) {
+        ints[i] = rnd.Next();
+    }
+
+    using(ParquetWriter writer = await ParquetWriter.CreateAsync(schema, ms)) {
+        using(ParquetRowGroupWriter g = writer.CreateRowGroup()) {
+            await g.WriteColumnAsync(new DataColumn((DataField)schema[0], ints));
+        }
     }
 }
