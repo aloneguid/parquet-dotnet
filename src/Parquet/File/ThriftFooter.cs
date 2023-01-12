43b2966e1aea8468304c90e8d69c975ad0b2f122
IG <aloneguid@outlook.com>
2023-01-12 11:26:50 +0000

    initial work on SchemaEncoder

--- src/Parquet/File/ThriftFooter.cs
+++ src/Parquet/File/ThriftFooter.cs
@@ -189,7 +189,10 @@ namespace Parquet.File {
                     throw new InvalidOperationException($"cannot find data type handler to create model schema for {tse.Describe()}");
                 }
 
+                Field se2 = SchemaEncoder.BuildField(_fileMeta.Schema, formatOptions, ref si, out int ownedChildCount2);
                 Field se = dth.CreateSchemaElement(_fileMeta.Schema, ref si, out int ownedChildCount);
+                if(!se.Equals(se2))
+                    throw new InvalidOperationException("migration failure");
 
                 se.Path = string.Join(ParquetSchema.PathSeparator, new[] { path, se.Path ?? se.Name }.Where(p => p != null));
 
--- src/Parquet/ParquetOptions.cs
+++ src/Parquet/ParquetOptions.cs
@@ -1,18 +1,23 @@
-﻿namespace Parquet
-{
-   /// <summary>
-   /// Parquet options
-   /// </summary>
-   public class ParquetOptions
-   {
-      /// <summary>
-      /// When true byte arrays will be treated as UTF-8 strings
-      /// </summary>
-      public bool TreatByteArrayAsString { get; set; } = false;
+﻿using System;
 
-      /// <summary>
-      /// Gets or sets a value indicating whether big integers are always treated as dates
-      /// </summary>
-      public bool TreatBigIntegersAsDates { get; set; } = true;
-   }
+namespace Parquet {
+    /// <summary>
+    /// Parquet options
+    /// </summary>
+    public class ParquetOptions {
+        /// <summary>
+        /// When true byte arrays will be treated as UTF-8 strings
+        /// </summary>
+        public bool TreatByteArrayAsString { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether big integers are always treated as dates
+        /// </summary>
+        public bool TreatBigIntegersAsDates { get; set; } = true;
+
+        /// <summary>
+        /// When true, date columns will be deserialized to <see cref="DateTime"/> instead of <see cref="DateTimeOffset"/>
+        /// </summary>
+        public bool PreferDateToDateTimeOffset { get; set; } = false;
+    }
 }
