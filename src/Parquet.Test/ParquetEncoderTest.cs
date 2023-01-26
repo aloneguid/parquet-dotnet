c2b9760ade0b281d9177af4a8fa310879ace327a
IG <aloneguid@outlook.com>
2023-01-26 14:12:12 +0000

    perform range checks when data page has more data than there is (#214)

--- src/Parquet.Test/ParquetEncoderTest.cs
+++ src/Parquet.Test/ParquetEncoderTest.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using Parquet.Data;
+using Xunit;
 
 namespace Parquet.Test {
-    class ParquetEncoderTest {
+    public class ParquetEncoderTest {
 
+        [Fact]
+        public void DecodeOutOfRangeSpan() {
+            var source = new MemoryStream(new byte[] {
+                64,  66,  15,  0, 0, 0, 0, 0,
+                128, 195, 201, 1, 0, 0, 0, 0,
+                1,   1,   1,   1, 1, 1, 1, 1, 1 });
 
+            var decoded = new TimeSpan[2];
+            ParquetPlainEncoder.Decode(source, decoded.AsSpan(), new Thrift.SchemaElement { Type = Thrift.Type.INT64 });
+
+            Assert.Equal(new TimeSpan(0, 0, 1), decoded[0]);
+            Assert.Equal(new TimeSpan(0, 0, 30), decoded[1]);
+        }
     }
 }
