96a7c729f70ff909104ec2cd899d2bfa59084489
IG <aloneguid@outlook.com>
2023-05-09 11:58:44 +0100

    add schema encoder test

--- src/Parquet.Test/Schema/SchemaEncoderTest.cs
+++ src/Parquet.Test/Schema/SchemaEncoderTest.cs
@@ -16,5 +16,15 @@ namespace Parquet.Test.Schema {
             Field f = SchemaEncoder.Decode(new List<Meta.SchemaElement> { se }, new ParquetOptions(), ref i, out _)!;
             Assert.Equal(typeof(decimal), ((DataField)f).ClrType);
         }
+
+        [Fact]
+        public void Encode_uuid() {
+            var uuid = new DataField<Guid>("u");
+            Meta.SchemaElement se = SchemaEncoder.Encode(uuid);
+
+            int i = 0;
+            Field f = SchemaEncoder.Decode(new List<Meta.SchemaElement> { se }, new ParquetOptions(), ref i, out _)!;
+            Assert.Equal(typeof(Guid), ((DataField)f).ClrType);
+        }
     }
 }
