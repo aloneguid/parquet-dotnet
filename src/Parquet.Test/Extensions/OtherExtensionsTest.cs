e056f4a60bba4432f4f7be4dc6e9e4fa74f2efc0
Paul Spangler <7519484+spanglerco@users.noreply.github.com>
2023-06-29 14:47:32 -0500

    Fix precision issues writing DateTime as milliseconds

---
+++ src/Parquet.Test/Extensions/OtherExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace Parquet.Test.Extensions {
+    public class OtherExtensionsTest {
+
+        [Theory]
+        [InlineData("1969-12-31T23:59:59.0001Z", -1000)]
+        [InlineData("1970-01-01T00:00:00.0001Z", 0)]
+        [InlineData("9999-12-31T23:59:59.999Z", 253402300799999)]
+        public void ToUnixMilliseconds_truncates_milliseconds_correctly(string dateString, long expectedMilliseconds) {
+            var dateTime = DateTime.Parse(dateString, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+
+            long actualMilliseconds = dateTime.ToUnixMilliseconds();
+
+            Assert.Equal(expectedMilliseconds, actualMilliseconds);
+        }
+    }
+}
