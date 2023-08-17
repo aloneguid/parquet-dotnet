7ab965163e2c63e215a6493ef997b2125d39d5c3
IG <aloneguid@outlook.com>
2023-08-17 11:34:48 +0100

    ULEB128 cleanup

--- src/Parquet.Test/Extensions/EncodingExtensionsTest.cs
+++ src/Parquet.Test/Extensions/EncodingExtensionsTest.cs
@@ -19,5 +19,24 @@ namespace Parquet.Test.Extensions {
 
             Assert.Equal(number, dec);
         }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(long.MinValue)]
+        [InlineData(long.MaxValue)]
+        public void ULEB128Test(long number) {
+            byte[] buf = new byte[10];
+            int offset1 = 0;
+            int offset2 = 0;
+            ((ulong)number).ULEB128Encode(buf, ref offset1);
+            ulong dec = ((Span<byte>)buf).ULEB128Decode(ref offset2);
+
+            Assert.Equal(offset1, offset1);
+            Assert.Equal(number, (long)dec);
+        }
     }
 }
