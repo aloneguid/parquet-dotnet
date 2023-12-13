90c9590df1062b4d3be2fed561c3eb9fa9af6d31
IG <aloneguid@outlook.com>
2023-12-13 22:53:53 +0000

    -3 nullable warnings in tests

--- src/Parquet.Test/Serialisation/SchemaReflectorTest.cs
+++ src/Parquet.Test/Serialisation/SchemaReflectorTest.cs
@@ -389,10 +389,9 @@ namespace Parquet.Test.Serialisation {
 
         public class StringPoco {
             [ParquetRequired]
-            public string Id { get; set; }
-
-            public string Name { get; set; }
-            public string Location { get; set; }
+            public string Id { get; set; } = "";
+            public string? Name { get; set; }
+            public string? Location { get; set; }
         }
 
         [Fact]
