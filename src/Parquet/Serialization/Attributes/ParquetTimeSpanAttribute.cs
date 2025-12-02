caae42ee68a52784b0c0655b226678952d7ba423
IG <aloneguid@outlook.com>
2025-12-02 15:18:03 +0000

    allow isAdjustedToUTC for timespan #650

---
+++ src/Parquet/Serialization/Attributes/ParquetTimeSpanAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parquet.Serialization.Attributes {
+
+    /// <summary>
+    /// Specifies that a property represents a time interval to be stored in Parquet format, with optional adjustment to
+    /// UTC.
+    /// </summary>
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ParquetTimeSpanAttribute : Attribute {
+        /// <summary>
+        /// IsAdjustedToUTC
+        /// </summary>
+        public bool IsAdjustedToUTC { get; set; } = true;
+    }
+}
