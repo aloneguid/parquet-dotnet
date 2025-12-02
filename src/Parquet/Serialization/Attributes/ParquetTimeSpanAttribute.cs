cdf8b57e3fcd9fe16044df4fe004375ad1036f77
IG <aloneguid@outlook.com>
2025-12-02 15:21:33 +0000

    file scoped namespaces for attributes

--- src/Parquet/Serialization/Attributes/ParquetTimeSpanAttribute.cs
+++ src/Parquet/Serialization/Attributes/ParquetTimeSpanAttribute.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Parquet.Serialization.Attributes {
+namespace Parquet.Serialization.Attributes; 
 
+/// <summary>
+/// Specifies that a property represents a time interval to be stored in Parquet format, with optional adjustment to
+/// UTC.
+/// </summary>
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class ParquetTimeSpanAttribute : Attribute {
     /// <summary>
-    /// Specifies that a property represents a time interval to be stored in Parquet format, with optional adjustment to
-    /// UTC.
+    /// IsAdjustedToUTC
     /// </summary>
-
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class ParquetTimeSpanAttribute : Attribute {
-        /// <summary>
-        /// IsAdjustedToUTC
-        /// </summary>
-        public bool IsAdjustedToUTC { get; set; } = true;
-    }
+    public bool IsAdjustedToUTC { get; set; } = true;
 }
--- src/Parquet/Serialization/Attributes/ParquetTimestampAttribute.cs
+++ src/Parquet/Serialization/Attributes/ParquetTimestampAttribute.cs
@@ -1,73 +1,72 @@
 ﻿using System;
 using Parquet.Schema;
 
-namespace Parquet.Serialization.Attributes {
+namespace Parquet.Serialization.Attributes; 
 
+
+/// <summary>
+/// Resolution of Parquet timestamp
+/// </summary>
+public enum ParquetTimestampResolution {
+    /// <summary>
+    /// Milliseconds, maps to <see cref="DateTimeFormat.DateAndTime"/>"
+    /// </summary>
+    Milliseconds,
+
+#if NET7_0_OR_GREATER
+    /// <summary>
+    /// Microseconds, maps to <see cref="DateTimeFormat.DateAndTimeMicros"/>"
+    /// </summary>
+    Microseconds
+#endif
+
+    // nanoseconds to be added
+}
+
+/// <summary>
+/// Specifies that a property of type <see cref="DateTime"/> should be serialized as Parquet timestamp, which is internally
+/// an int64 number.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class ParquetTimestampAttribute : Attribute {
+
+    /// <summary>
+    /// Creates an instance of the attribute
+    /// </summary>
+    /// <param name="resolution"></param>
+    /// <param name="useLogicalTimestamp"></param>
+    /// <param name="isAdjustedToUTC"></param>
+    public ParquetTimestampAttribute(ParquetTimestampResolution resolution = ParquetTimestampResolution.Milliseconds, bool useLogicalTimestamp = false, bool isAdjustedToUTC = true) {
+        Resolution = resolution;
+        UseLogicalTimestamp = useLogicalTimestamp;
+        IsAdjustedToUTC = isAdjustedToUTC;
+    }
 
     /// <summary>
     /// Resolution of Parquet timestamp
     /// </summary>
-    public enum ParquetTimestampResolution {
-        /// <summary>
-        /// Milliseconds, maps to <see cref="DateTimeFormat.DateAndTime"/>"
-        /// </summary>
-        Milliseconds,
-
-#if NET7_0_OR_GREATER
-        /// <summary>
-        /// Microseconds, maps to <see cref="DateTimeFormat.DateAndTimeMicros"/>"
-        /// </summary>
-        Microseconds
-#endif
-
-        // nanoseconds to be added
-    }
-
+    public ParquetTimestampResolution Resolution { get; private set; }
+    
     /// <summary>
-    /// Specifies that a property of type <see cref="DateTime"/> should be serialized as Parquet timestamp, which is internally
-    /// an int64 number.
+    /// Resolution of Parquet timestamp
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class ParquetTimestampAttribute : Attribute {
+    public bool UseLogicalTimestamp { get; private set; }
+    
+    /// <summary>
+    /// IsAdjustedToUTC
+    /// </summary>
+    public bool IsAdjustedToUTC { get; private set; }
 
-        /// <summary>
-        /// Creates an instance of the attribute
-        /// </summary>
-        /// <param name="resolution"></param>
-        /// <param name="useLogicalTimestamp"></param>
-        /// <param name="isAdjustedToUTC"></param>
-        public ParquetTimestampAttribute(ParquetTimestampResolution resolution = ParquetTimestampResolution.Milliseconds, bool useLogicalTimestamp = false, bool isAdjustedToUTC = true) {
-            Resolution = resolution;
-            UseLogicalTimestamp = useLogicalTimestamp;
-            IsAdjustedToUTC = isAdjustedToUTC;
-        }
-
-        /// <summary>
-        /// Resolution of Parquet timestamp
-        /// </summary>
-        public ParquetTimestampResolution Resolution { get; private set; }
+    internal DateTimeFormat GetDateTimeFormat() {
+        if(UseLogicalTimestamp)
+            return DateTimeFormat.Timestamp;
         
-        /// <summary>
-        /// Resolution of Parquet timestamp
-        /// </summary>
-        public bool UseLogicalTimestamp { get; private set; }
-        
-        /// <summary>
-        /// IsAdjustedToUTC
-        /// </summary>
-        public bool IsAdjustedToUTC { get; private set; }
-
-        internal DateTimeFormat GetDateTimeFormat() {
-            if(UseLogicalTimestamp)
-                return DateTimeFormat.Timestamp;
-            
-            return Resolution switch {
-                ParquetTimestampResolution.Milliseconds => DateTimeFormat.DateAndTime,
+        return Resolution switch {
+            ParquetTimestampResolution.Milliseconds => DateTimeFormat.DateAndTime,
 #if NET7_0_OR_GREATER
-                ParquetTimestampResolution.Microseconds => DateTimeFormat.DateAndTimeMicros,
+            ParquetTimestampResolution.Microseconds => DateTimeFormat.DateAndTimeMicros,
 #endif
-                _ => throw new NotSupportedException($"Timestamp resolution {Resolution} is not supported")
-            };
-        }
+            _ => throw new NotSupportedException($"Timestamp resolution {Resolution} is not supported")
+        };
     }
 }
