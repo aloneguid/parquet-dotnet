29ec869204c83516b416a7e178d30554c21e473e
IG <aloneguid@outlook.com>
2025-12-08 22:32:57 +0000

    dfdsf

--- src/Parquet.Test/CompressionTest.cs
+++ src/Parquet.Test/CompressionTest.cs
@@ -3,42 +3,42 @@ using Parquet.Data;
 using Parquet.Schema;
 using Xunit;
 
-namespace Parquet.Test {
-    public class CompressionTest : TestBase {
-        [Theory]
-        [InlineData(CompressionMethod.None)]
-        [InlineData(CompressionMethod.Snappy)]
-        [InlineData(CompressionMethod.Gzip)]
-        [InlineData(CompressionMethod.Lzo)]
-        [InlineData(CompressionMethod.Brotli)]
-        [InlineData(CompressionMethod.LZ4)]
-        [InlineData(CompressionMethod.Zstd)]
-        [InlineData(CompressionMethod.Lz4Raw)]
-        public async Task All_compression_methods_supported_for_simple_integeres(CompressionMethod compressionMethod) {
-            const int value = 5;
-            object actual = await WriteReadSingle(new DataField<int>("id"), value, compressionMethod);
-            Assert.Equal(5, (int)actual);
-        }
+namespace Parquet.Test; 
 
-        [Theory]
-        [InlineData(CompressionMethod.None)]
-        [InlineData(CompressionMethod.Snappy)]
-        [InlineData(CompressionMethod.Gzip)]
-        [InlineData(CompressionMethod.Lzo)]
-        [InlineData(CompressionMethod.Brotli)]
-        [InlineData(CompressionMethod.LZ4)]
-        [InlineData(CompressionMethod.Zstd)]
-        [InlineData(CompressionMethod.Lz4Raw)]
-        public async Task All_compression_methods_supported_for_simple_strings(CompressionMethod compressionMethod) {
-            /*
-             * uncompressed: length - 14, levels - 6
-             * 
-             * 
-             */
+public class CompressionTest : TestBase {
+    [Theory]
+    [InlineData(CompressionMethod.None)]
+    [InlineData(CompressionMethod.Snappy)]
+    [InlineData(CompressionMethod.Gzip)]
+    //[InlineData(CompressionMethod.Lzo)]
+    [InlineData(CompressionMethod.Brotli)]
+    [InlineData(CompressionMethod.LZ4)]
+    [InlineData(CompressionMethod.Zstd)]
+    [InlineData(CompressionMethod.Lz4Raw)]
+    public async Task All_compression_methods_supported_for_simple_integers(CompressionMethod compressionMethod) {
+        const int value = 5;
+        object actual = await WriteReadSingle(new DataField<int>("id"), value, compressionMethod);
+        Assert.Equal(5, (int)actual);
+    }
 
-            const string value = "five";
-            object actual = await WriteReadSingle(new DataField<string>("id"), value, compressionMethod);
-            Assert.Equal("five", actual);
-        }
+    [Theory]
+    [InlineData(CompressionMethod.None)]
+    [InlineData(CompressionMethod.Snappy)]
+    [InlineData(CompressionMethod.Gzip)]
+    //[InlineData(CompressionMethod.Lzo)]
+    [InlineData(CompressionMethod.Brotli)]
+    [InlineData(CompressionMethod.LZ4)]
+    [InlineData(CompressionMethod.Zstd)]
+    [InlineData(CompressionMethod.Lz4Raw)]
+    public async Task All_compression_methods_supported_for_simple_strings(CompressionMethod compressionMethod) {
+        /*
+         * uncompressed: length - 14, levels - 6
+         * 
+         * 
+         */
+
+        const string value = "five";
+        object actual = await WriteReadSingle(new DataField<string>("id"), value, compressionMethod);
+        Assert.Equal("five", actual);
     }
 }
