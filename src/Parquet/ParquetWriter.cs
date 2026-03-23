8a43446ad3aa176c677b3230c5ee10ae7441e3e4
aloneguid <aloneguid@outlook.com>
2026-03-23 12:25:37 +0000

    versioning logic

--- src/Parquet/ParquetWriter.cs
+++ src/Parquet/ParquetWriter.cs
@@ -11,160 +11,162 @@ using Parquet.Meta;
 using Parquet.Extensions;
 using Parquet.Extensions.Streaming;
 
-namespace Parquet {
+namespace Parquet; 
+
+/// <summary>
+/// Implements Apache Parquet format writer
+/// </summary>
+public sealed class ParquetWriter : ParquetActor, IDisposable, IAsyncDisposable {
+    private ThriftFooter? _footer;
+    private readonly ParquetSchema _schema;
+    private readonly ParquetOptions _formatOptions;
+    private bool _dataWritten;
+    private readonly List<ParquetRowGroupWriter> _openedWriters = new List<ParquetRowGroupWriter>();
+
     /// <summary>
-    /// Implements Apache Parquet format writer
+    /// Type of compression to use, defaults to <see cref="CompressionMethod.Snappy"/>
     /// </summary>
-    public sealed class ParquetWriter : ParquetActor, IDisposable, IAsyncDisposable {
-        private ThriftFooter? _footer;
-        private readonly ParquetSchema _schema;
-        private readonly ParquetOptions _formatOptions;
-        private bool _dataWritten;
-        private readonly List<ParquetRowGroupWriter> _openedWriters = new List<ParquetRowGroupWriter>();
+    public CompressionMethod CompressionMethod { get; set; } = CompressionMethod.Snappy;
 
-        /// <summary>
-        /// Type of compression to use, defaults to <see cref="CompressionMethod.Snappy"/>
-        /// </summary>
-        public CompressionMethod CompressionMethod { get; set; } = CompressionMethod.Snappy;
-
-        /// <summary>
-        /// Level of compression
-        /// </summary>
+    /// <summary>
+    /// Level of compression
+    /// </summary>
 #if NET6_0_OR_GREATER
-        public CompressionLevel CompressionLevel = CompressionLevel.SmallestSize;
+    public CompressionLevel CompressionLevel = CompressionLevel.SmallestSize;
 #else
-        public CompressionLevel CompressionLevel = CompressionLevel.Optimal;
+    public CompressionLevel CompressionLevel = CompressionLevel.Optimal;
 #endif
 
-        private ParquetWriter(ParquetSchema schema, Stream output, ParquetOptions? formatOptions = null, bool append = false)
-           : base(output.CanSeek == true ? output : new MeteredWriteStream(output)) {
-            if(output == null)
-                throw new ArgumentNullException(nameof(output));
+    private ParquetWriter(ParquetSchema schema, Stream output, ParquetOptions? formatOptions = null, bool append = false)
+       : base(output.CanSeek == true ? output : new MeteredWriteStream(output)) {
+        if(output == null)
+            throw new ArgumentNullException(nameof(output));
 
-            if(!output.CanWrite)
-                throw new ArgumentException("stream is not writeable", nameof(output));
-            _schema = schema ?? throw new ArgumentNullException(nameof(schema));
-            _formatOptions = formatOptions ?? new ParquetOptions();
-        }
+        if(!output.CanWrite)
+            throw new ArgumentException("stream is not writeable", nameof(output));
+        _schema = schema ?? throw new ArgumentNullException(nameof(schema));
+        _formatOptions = formatOptions ?? new ParquetOptions();
+    }
 
-        /// <summary>
-        /// Creates an instance of parquet writer on top of a stream
-        /// </summary>
-        /// <param name="schema"></param>
-        /// <param name="output">Writeable, seekable stream</param>
-        /// <param name="formatOptions">Additional options</param>
-        /// <param name="append"></param>
-        /// <param name="cancellationToken"></param>
-        /// <exception cref="ArgumentNullException">Output is null.</exception>
-        /// <exception cref="ArgumentException">Output stream is not writeable</exception>
-        public static async Task<ParquetWriter> CreateAsync(
-            ParquetSchema schema, Stream output, ParquetOptions? formatOptions = null, bool append = false,
-            CancellationToken cancellationToken = default) {
-            var writer = new ParquetWriter(schema, output, formatOptions, append);
-            await writer.PrepareFileAsync(append, cancellationToken);
-            return writer;
-        }
+    /// <summary>
+    /// Creates an instance of parquet writer on top of a stream. dfaldkjf ;adkfj ad;lfja ;ldjflkjlkj kjflkjdlfj jlf
+    /// ldsjf ljdlsfjk adljflkdj fldjfljd ;sfj. lkdjf;j ;jfdsjk;afjdlkjfdlsjflkdjsfdsf. dlkj;lj ; j;j ;lj
+    /// dslfkj;jf;ljkflj d;fljka ;djf ;jd f;laj dlfj f fdsljf lj.
+    /// </summary>
+    /// <param name="schema"></param>
+    /// <param name="output">Writeable, seekable stream</param>
+    /// <param name="formatOptions">Additional options</param>
+    /// <param name="append"></param>
+    /// <param name="cancellationToken"></param>
+    /// <exception cref="ArgumentNullException">Output is null.</exception>
+    /// <exception cref="ArgumentException">Output stream is not writeable</exception>
+    public static async Task<ParquetWriter> CreateAsync(
+        ParquetSchema schema, Stream output, ParquetOptions? formatOptions = null, bool append = false,
+        CancellationToken cancellationToken = default) {
+        var writer = new ParquetWriter(schema, output, formatOptions, append);
+        await writer.PrepareFileAsync(append, cancellationToken);
+        return writer;
+    }
 
-        /// <summary>
-        /// Creates a new row group and a writer for it.
-        /// </summary>
-        public ParquetRowGroupWriter CreateRowGroup() {
-            _dataWritten = true;
+    /// <summary>
+    /// Creates a new row group and a writer for it.
+    /// </summary>
+    public ParquetRowGroupWriter CreateRowGroup() {
+        _dataWritten = true;
 
-            var writer = new ParquetRowGroupWriter(_schema, Stream, _footer!,
-               CompressionMethod, _formatOptions, CompressionLevel);
+        var writer = new ParquetRowGroupWriter(_schema, Stream, _footer!,
+           CompressionMethod, _formatOptions, CompressionLevel);
 
-            _openedWriters.Add(writer);
+        _openedWriters.Add(writer);
 
-            return writer;
-        }
+        return writer;
+    }
 
-        /// <summary>
-        /// Gets custom key-value pairs for metadata
-        /// </summary>
-        public IReadOnlyDictionary<string, string> CustomMetadata {
-            get => _footer!.CustomMetadata;
-            set => _footer!.CustomMetadata = value.ToDictionary(p => p.Key, p => p.Value);
-        }
+    /// <summary>
+    /// Gets custom key-value pairs for metadata
+    /// </summary>
+    public IReadOnlyDictionary<string, string> CustomMetadata {
+        get => _footer!.CustomMetadata;
+        set => _footer!.CustomMetadata = value.ToDictionary(p => p.Key, p => p.Value);
+    }
 
-        private async Task PrepareFileAsync(bool append, CancellationToken cancellationToken) {
-            if(append) {
-                if(!Stream.CanSeek)
-                    throw new IOException("destination stream must be seekable for append operations.");
+    private async Task PrepareFileAsync(bool append, CancellationToken cancellationToken) {
+        if(append) {
+            if(!Stream.CanSeek)
+                throw new IOException("destination stream must be seekable for append operations.");
 
-                if(Stream.Length == 0)
-                    throw new IOException($"you can only append to existing streams, but current stream is empty.");
+            if(Stream.Length == 0)
+                throw new IOException($"you can only append to existing streams, but current stream is empty.");
 
-                await ValidateFileAsync();
+            await ValidateFileAsync();
 
-                FileMetaData fileMeta = await ReadMetadataAsync(cancellationToken);
-                _footer = new ThriftFooter(fileMeta);
+            FileMetaData fileMeta = await ReadMetadataAsync(cancellationToken);
+            _footer = new ThriftFooter(fileMeta);
 
+            ValidateSchemasCompatible(_footer, _schema);
+
+            await GoBeforeFooterAsync();
+        } else {
+            if(_footer == null) {
+                _footer = new ThriftFooter(_schema, 0 /* todo: don't forget to set the total row count at the end!!! */, _formatOptions);
+
+                //file starts with magic
+                await WriteMagicAsync();
+            } else {
                 ValidateSchemasCompatible(_footer, _schema);
 
-                await GoBeforeFooterAsync();
-            } else {
-                if(_footer == null) {
-                    _footer = new ThriftFooter(_schema, 0 /* todo: don't forget to set the total row count at the end!!! */, _formatOptions);
-
-                    //file starts with magic
-                    await WriteMagicAsync();
-                } else {
-                    ValidateSchemasCompatible(_footer, _schema);
-
-                    _footer.Add(0 /* todo: don't forget to set the total row count at the end!!! */);
-                }
+                _footer.Add(0 /* todo: don't forget to set the total row count at the end!!! */);
             }
         }
+    }
 
-        private void ValidateSchemasCompatible(ThriftFooter footer, ParquetSchema schema) {
-            ParquetSchema existingSchema = footer.CreateModelSchema(_formatOptions);
+    private void ValidateSchemasCompatible(ThriftFooter footer, ParquetSchema schema) {
+        ParquetSchema existingSchema = footer.CreateModelSchema(_formatOptions);
 
-            if(!schema.Equals(existingSchema)) {
-                string reason = schema.GetNotEqualsMessage(existingSchema, "appending", "existing");
-                throw new ParquetException($"passed schema does not match existing file schema, reason: {reason}");
-            }
+        if(!schema.Equals(existingSchema)) {
+            string reason = schema.GetNotEqualsMessage(existingSchema, "appending", "existing");
+            throw new ParquetException($"passed schema does not match existing file schema, reason: {reason}");
         }
+    }
 
-        private void WriteMagic() => Stream.Write(MagicBytes, 0, MagicBytes.Length);
-        private Task WriteMagicAsync() => Stream.WriteAsync(MagicBytes, 0, MagicBytes.Length);
+    private void WriteMagic() => Stream.Write(MagicBytes, 0, MagicBytes.Length);
+    private Task WriteMagicAsync() => Stream.WriteAsync(MagicBytes, 0, MagicBytes.Length);
 
-        private void DisposeCore() {
-            if(_dataWritten) {
-                //update row count (on append add row count to existing metadata)
-                _footer!.Add(_openedWriters.Sum(w => w.RowCount ?? 0));
-            }
+    private void DisposeCore() {
+        if(_dataWritten) {
+            //update row count (on append add row count to existing metadata)
+            _footer!.Add(_openedWriters.Sum(w => w.RowCount ?? 0));
         }
+    }
 
-        /// <summary>
-        /// Disposes the writer and writes the file footer.
-        /// </summary>
-        public void Dispose() {
+    /// <summary>
+    /// Disposes the writer and writes the file footer.
+    /// </summary>
+    public void Dispose() {
 
-            DisposeCore();
+        DisposeCore();
 
-            if(_footer == null)
-                return;
+        if(_footer == null)
+            return;
 
-            long size = _footer.Write(Stream);
-            Stream.WriteInt32((int)size); // metadata size: 4 bytes
-            WriteMagic();                 // end magic:     4 bytes
-            Stream.Flush();
-        }
+        long size = _footer.Write(Stream);
+        Stream.WriteInt32((int)size); // metadata size: 4 bytes
+        WriteMagic();                 // end magic:     4 bytes
+        Stream.Flush();
+    }
 
-        /// <summary>
-        /// Dispose the writer asynchronously
-        /// </summary>
-        public async ValueTask DisposeAsync() {
-            DisposeCore();
+    /// <summary>
+    /// Dispose the writer asynchronously
+    /// </summary>
+    public async ValueTask DisposeAsync() {
+        DisposeCore();
 
-            if(_footer == null)
-                return;
+        if(_footer == null)
+            return;
 
-            long size = await _footer.WriteAsync(Stream).ConfigureAwait(false);
-            await Stream.WriteInt32Async((int)size);
-            await WriteMagicAsync();
-            await Stream.FlushAsync();
-        }
+        long size = await _footer.WriteAsync(Stream).ConfigureAwait(false);
+        await Stream.WriteInt32Async((int)size);
+        await WriteMagicAsync();
+        await Stream.FlushAsync();
     }
 }
